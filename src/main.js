a40489ad8b611320eec6901c0b4ab58804a0477b
Krasimira Georgieva <krassi77bg@gmail.com>
2019-08-20 22:43:17 +0300

    Add route to template, add main photo,

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
+import Vuelidate from "vuelidate";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
+Vue.use(Vuelidate);
 
 new Vue({
   router,
--- src/router.js
+++ src/router.js
@@ -1,10 +1,17 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Home from "@/components/home/Welcome";
+import MyListings from "@/components/home/MyListings";
+import Register from "@/components/forms/RegisterForm";
+import Login from "@/components/forms/LoginForm";
+import Create from "@/components/forms/CreateForm";
+import Details from "@/components/car/DetailsCar";
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes: [
     {
       path: "/",
@@ -12,13 +19,29 @@ export default new Router({
       component: Home
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
-    }
+      path: "/register",
+      name: "register",
+      component: Register
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login
+    },
+    {
+      path: "/create",
+      name: "create",
+      component: Create
+    },
+    {
+      path: "/details/:id",
+      name: "details",
+      component: Details
+    },
+    {
+      path: "/my-listings",
+      name: "myListings",
+      component: MyListings
+    },
   ]
 });
